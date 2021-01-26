46cbe7441baee55388bc34725263cd5ef9aaaaf4
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-01-27 00:47:47 +0300

    add group class

--- include/Group.h
+++ include/Group.h
@@ -1,11 +1,44 @@
-//
-// Created by stanislav on 19/01/2021.
-//
+// Copyright 2020 Stanislav Stoianov
 
 #ifndef TP_LAB_5_INCLUDE_GROUP_H_
 #define TP_LAB_5_INCLUDE_GROUP_H_
 
+#include <string>
+#include <vector>
+#include "Student.h"
+
+class Student;
+
 class Group {
+ private:
+  std::string title;
+  std::string spec;
+  std::vector<Student*> students;
+  Student *head{};
+ public:
+  Group(std::string t);
+
+  Group(std::string t, std::string s);
+
+  void addStudent(Student* student);
+
+  void chooseHead(Student * student);
+
+  Student *containsStudent(int id);
+
+  Student* containsStudent(const std::string &fio);
+
+  double getAverageMark();
+
+  void removeStudent(Student *student);
+
+  const std::string &getTitle() const;
+
+  const std::string &getSpec() const;
+
+  const std::vector<Student *> &getStudents() const;
+
+  Student *getHead() const;
 
 };
 
--- src/Group.cpp
+++ src/Group.cpp
@@ -1,5 +1,69 @@
-//
-// Created by stanislav on 19/01/2021.
-//
+// Copyright 2020 Stanislav Stoianov
 
+#include <algorithm>
+#include <utility>
 #include "../include/Group.h"
+
+Group::Group(std::string t) {
+  this->title = std::move(t);
+}
+
+Group::Group(std::string t, std::string s) {
+  this->title = std::move(t);
+  this->spec = std::move(s);
+}
+
+void Group::addStudent(Student *student) {
+  student->addToGroup(this);
+  this->students.push_back(student);
+}
+
+void Group::chooseHead(Student *student) {
+  this->head = student;
+}
+
+Student *Group::containsStudent(int id) {
+  for (Student *student : this->students) {
+    if (student->getId() == id) return student;
+  }
+  throw std::invalid_argument("Student does not exist");
+}
+
+Student *Group::containsStudent(const std::string &fio) {
+  for (Student *student : this->students) {
+    if (student->getFullname() == fio) return student;
+  }
+  throw std::invalid_argument("Student does not exist");
+}
+
+double Group::getAverageMark() {
+  double accum = 0.0;
+  for (Student *student : this->students) {
+    accum += student->getAverageMark();
+  }
+  return this->students.empty() ? 0.0 : accum / this->students.size();
+}
+
+void Group::removeStudent(Student *student) {
+  if (this->head == student) this->head = nullptr;
+  this->students.erase(
+      std::remove(this->students.begin(), this->students.end(), student),
+      this->students.end());
+  student->addToGroup(nullptr);
+}
+
+const std::string &Group::getTitle() const {
+  return title;
+}
+
+const std::string &Group::getSpec() const {
+  return spec;
+}
+
+Student *Group::getHead() const {
+  return head;
+}
+
+const std::vector<Student *> &Group::getStudents() const {
+  return students;
+}
