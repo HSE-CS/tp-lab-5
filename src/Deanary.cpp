9c8a402a3324a0bd60390b3793715d090211cbcc
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-01-27 01:44:33 +0300

    add several functions

--- src/Deanary.cpp
+++ src/Deanary.cpp
@@ -1,3 +1,18 @@
 // Copyright 2020 Stanislav Stoianov
 
 #include "../include/Deanary.h"
+
+void Deanery::addMarksToAll() {
+  for (Group *g : groups) {
+    for (Student *s : g->getStudents()) {
+      s->addMark(rand() % 10);
+    }
+  }
+}
+
+void Deanery::initHeads() {
+  for (Group *g : groups) {
+    g->chooseHead(g->getStudents()
+                      .at(rand() % g->getStudents().size()));
+  }
+}
