26bdf05f6605af40194d3877bb29b9cf2d88ab53
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-01-27 00:47:35 +0300

    add student class

--- include/Student.h
+++ include/Student.h
@@ -1,11 +1,40 @@
-//
-// Created by stanislav on 19/01/2021.
-//
+// Copyright 2020 Stanislav Stoianov
 
 #ifndef TP_LAB_5_INCLUDE_STUDENT_H_
 #define TP_LAB_5_INCLUDE_STUDENT_H_
 
+#include <string>
+#include <vector>
+#include "Group.h"
+
+class Group;
+
 class Student {
+ private:
+  int id;
+  std::string fullname;
+  Group *group;
+  std::vector<unsigned short> marks;
+ public:
+  Student(int id, std::string fullname);
+
+  ~Student();
+
+  void addToGroup(Group *g);
+
+  void addMark(unsigned short mark);
+
+  double getAverageMark();
+
+  bool isHeadOfGroup();
+
+  int getId() const;
+
+  const std::string &getFullname() const;
+
+  Group *getGroup() const;
+
+  const std::vector<unsigned short> &getMarks() const;
 
 };
 
--- src/Student.cpp
+++ src/Student.cpp
@@ -1,5 +1,53 @@
-//
-// Created by stanislav on 19/01/2021.
-//
+// Copyright 2020 Stanislav Stoianov
 
 #include "../include/Student.h"
+#include <utility>
+
+Student::Student(int id, std::string fullname) {
+  this->id = id;
+  this->fullname = std::move(fullname);
+  this->group = nullptr;
+}
+
+Student::~Student() {
+  this->id = 0;
+  this->fullname.clear();
+  this->group = nullptr;
+  this->marks.clear();
+}
+
+void Student::addToGroup(Group *g) {
+  this->group = g;
+}
+
+void Student::addMark(unsigned short mark) {
+  marks.push_back(mark);
+}
+
+double Student::getAverageMark() {
+  double accum = 0.0;
+  for (unsigned int mark : this->marks) {
+    accum += mark;
+  }
+  return this->marks.empty() ? 0.0 : accum / this->marks.size();
+}
+
+bool Student::isHeadOfGroup() {
+  return this->group != nullptr && this->group->getHead() == this;
+}
+
+int Student::getId() const {
+  return id;
+}
+
+const std::string &Student::getFullname() const {
+  return fullname;
+
+}
+Group *Student::getGroup() const {
+  return group;
+}
+
+const std::vector<unsigned short> &Student::getMarks() const {
+  return marks;
+}
