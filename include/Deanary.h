fceec682dbc9a26cd5bad8430e509dc86cd0c889
VadMack <makarovvadim01@icloud.com>
2021-02-01 09:32:36 +0300

    Deanary.h style fix

--- include/Deanary.h
+++ include/Deanary.h
@@ -1,28 +1,27 @@
 // Copyright 2021 VadMack
 
-#ifndef INCLUDE_DEANARY_H
-#define INCLUDE_DEANARY_H
+#ifndef INCLUDE_DEANARY_H_
+#define INCLUDE_DEANARY_H_
 
 #include "Group.h"
 
 class Student;
-class Deanary{
+class Deanary {
+#include <vector>
  private:
-  //Group **groups;
   std::vector<Group *> groups;
 
  public:
   void createGroups();
   static std::vector<Student *> createStudents();
-  static void moveStudents(std::vector<Student *> students, Group *from, Group *to);
+  static void moveStudents(std::vector<Student *> students,
+                           Group *from,
+                           Group *to);
   void addRandomMarksToAll();
   void initHeads();
   void getStatistics();
   void saveStaff();
-  //Group **getGroups() const;
   const std::vector<Group *> &getGroups() const;
-
 };
 
-
-#endif // INCLUDE_DEANARY_H
+#endif //  INCLUDE_DEANARY_H_
