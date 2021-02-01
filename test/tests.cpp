94c99f146d0f6b9410da9c4a9afbc11d32c786b4
VadMack <makarovvadim01@icloud.com>
2021-02-01 13:03:25 +0300

    first 4 tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,5 +2,31 @@
 
 #include <gtest/gtest.h>
 #include <string>
+#include "../include/Student.h"
+#include "../include/Group.h"
+#include "../include/Deanary.h"
+
+TEST(PostfixTest, test1) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  EXPECT_STREQ("1", student.getId);
+}
+
+TEST(PostfixTest, test2) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  EXPECT_STREQ(student.getFio, "Pavlov pavel Pavlovich");
+}
+
+TEST(PostfixTest, test3) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  EXPECT_STREQ(nullptr, student.getGroup);
+}
+
+TEST(PostfixTest, test4) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  Group group1("19SE-1", "SE");
+  student1.addToGroup(&group1);
+  EXPECT_STREQ("19SE-1", student.getGroup()->GetTitle());
+}
+
 
 
