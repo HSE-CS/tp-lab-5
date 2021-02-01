51a0cdb3db25c808892eafe5411a2a0af0c651e6
VadMack <makarovvadim01@icloud.com>
2021-02-01 13:22:11 +0300

    tests fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -14,7 +14,8 @@ TEST(PostfixTest, test1) {
 
 TEST(PostfixTest, test2) {
   Student student("1", "Pavlov pavel Pavlovich");
-  EXPECT_EQ(student.getFio, "Pavlov pavel Pavlovich");
+  std::string fio = student.getFio();
+  EXPECT_EQ("Pavlov pavel Pavlovich", fio);
 }
 
 TEST(PostfixTest, test3) {
@@ -25,7 +26,7 @@ TEST(PostfixTest, test3) {
 TEST(PostfixTest, test4) {
   Student student("1", "Pavlov pavel Pavlovich");
   Group group1("19SE-1", "SE");
-  student1.addToGroup(&group1);
+  student.addToGroup(&group1);
   EXPECT_EQ("19SE-1", student.getGroup()->GetTitle());
 }
 
