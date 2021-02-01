81a4eb6ec18016978afe6e025d51174aca7f39cb
VadMack <makarovvadim01@icloud.com>
2021-02-01 13:59:57 +0300

    added remaining tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -18,16 +18,60 @@ TEST(PostfixTest, test2) {
   EXPECT_EQ("Pavlov pavel Pavlovich", fio);
 }
 
+
 TEST(PostfixTest, test3) {
   Student student("1", "Pavlov pavel Pavlovich");
-  EXPECT_EQ("", student.getGroup()->GetTitle());
+  Group group1("19SE-1", "SE");
+  student.addToGroup(&group1);
+  EXPECT_EQ("19SE-1", student.getGroup()->GetTitle());
 }
 
 TEST(PostfixTest, test4) {
   Student student("1", "Pavlov pavel Pavlovich");
   Group group1("19SE-1", "SE");
   student.addToGroup(&group1);
+  EXPECT_EQ("Pavlov pavel Pavlovich", group1.getStudents()[0].getFio());
+}
+
+TEST(PostfixTest, test5) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  Group group1("19SE-1", "SE");
+  group1.addStudent(&student);
   EXPECT_EQ("19SE-1", student.getGroup()->GetTitle());
+}
+
+TEST(PostfixTest, test6) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  Group group1("19SE-1", "SE");
+  group1.addStudent(&student);
+  EXPECT_EQ("Pavlov pavel Pavlovich", group1.getStudents()[0].getFio());
+}
+
+TEST(PostfixTest, test7) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  student.addMark(10);
+  EXPECT_EQ(10, student.getMarks()[0]);
+}
+
+TEST(PostfixTest, test8) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  Group group1("19SE-1", "SE");
+  group1.addStudent(&student);
+  group1.chooseHead(&student);
+  EXPECT_EQ("Pavlov pavel Pavlovich", group1.getHead()->getFio());
+}
+
+TEST(PostfixTest, test9) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  student.addMark(10);
+  EXPECT_EQ(10, student.calculateAverageMark());
+}
+
+TEST(PostfixTest, test10) {
+  Student student("1", "Pavlov pavel Pavlovich");
+  student.addMark(10);
+  student.addMark(9);
+  EXPECT_EQ(9.5, student.calculateAverageMark());
 }
 
 
