2b0e978069cb51f179d35b43a9b1f611c44f3770
JGMax <maksus20.05@mail.ru>
2021-02-01 19:49:11 +0300

    Lets try

---
+++ include/Group.h
@@ -0,0 +1,43 @@
+// Copyright by JMax 2020
+
+#ifndef INCLUDE_GROUP_H_
+#define INCLUDE_GROUP_H_
+
+#include <string>
+#include <vector>
+#include "Deanary.h"
+#include "Student.h"
+
+class Student;
+
+class Group {
+    friend class Deanary;
+ private:
+    std::string title;
+    std::string spec;
+    std::vector<Student*> students;
+    Student* head;
+
+    void addStudent(Student* student);
+    void chooseHead();
+    void removeStudent(Student* student);
+    void removeStudent(unsigned int id);
+    void removeStudent(const std::string& fio);
+    int studentIndex(unsigned int id) const;
+    int studentIndex(const std::string& fio) const;
+
+ public:
+    Group(const std::string& title, const std::string& spec);
+    ~Group();
+    double getAverageMark() const;
+    Student* getStudent(unsigned int id) const;
+    Student* getStudent(const std::string& fio) const;
+    bool containsStudent(unsigned int id) const;
+    bool containsStudent(const std::string& fio) const;
+    bool isEmpty() const;
+    unsigned int numOfStudents() const;
+    Student* getHead() const;
+    void setHead(const std::string& fio);
+};
+
+#endif // INCLUDE_GROUP_H_
---
+++ src/Student.cpp
@@ -0,0 +1,55 @@
+// Copyright by JMax 2020
+
+#include "Student.h"
+
+
+Student::Student(const unsigned int id, const std::string &fio) {
+    this->id = id;
+    this->fio = fio;
+    this->marks = std::vector<unsigned int>();
+}
+
+void Student::addToGroup(Group *group) {
+    this->group = group;
+}
+
+void Student::removeFromGroup() {
+    group = nullptr;
+}
+
+void Student::addMark(unsigned int mark) {
+    marks.push_back(mark);
+}
+
+double Student::getAverageMark() {
+    return (double)sumOfMarks()/numOfMarks();
+}
+
+unsigned int Student::sumOfMarks() {
+    unsigned int sum = 0;
+    for (auto mark : marks) {
+        sum += mark;
+    }
+    return sum;
+}
+
+unsigned int Student::numOfMarks() {
+    return marks.size();
+}
+
+bool Student::isHeadOfGroup() {
+    if (group != nullptr) {
+        return group->getHead() == this;
+    } else {
+        return false;
+    }
+}
+
+std::string Student::getFio() const {
+    return fio;
+}
+
+unsigned int Student::getId() const {
+    return id;
+}
+
