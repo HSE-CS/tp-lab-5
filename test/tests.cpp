de16c56f6f0f00bc0217455e21ed21aa4a0dd471
VadMack <makarovvadim01@icloud.com>
2021-02-01 13:06:34 +0300

    tests fix

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,24 +8,24 @@
 
 TEST(PostfixTest, test1) {
   Student student("1", "Pavlov pavel Pavlovich");
-  EXPECT_STREQ("1", student.getId);
+  ASSERT_STREQ("1", student.getId);
 }
 
 TEST(PostfixTest, test2) {
   Student student("1", "Pavlov pavel Pavlovich");
-  EXPECT_STREQ(student.getFio, "Pavlov pavel Pavlovich");
+  ASSERT_STREQ(student.getFio, "Pavlov pavel Pavlovich");
 }
 
 TEST(PostfixTest, test3) {
   Student student("1", "Pavlov pavel Pavlovich");
-  EXPECT_STREQ(nullptr, student.getGroup);
+  ASSERT_STREQ(nullptr, student.getGroup);
 }
 
 TEST(PostfixTest, test4) {
   Student student("1", "Pavlov pavel Pavlovich");
   Group group1("19SE-1", "SE");
   student1.addToGroup(&group1);
-  EXPECT_STREQ("19SE-1", student.getGroup()->GetTitle());
+  ASSERT_STREQ("19SE-1", student.getGroup()->GetTitle());
 }
 
 
