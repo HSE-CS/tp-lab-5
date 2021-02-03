db5c637712e8ded98492c49dc124619900117770
BekusovMikhail <mabekusov@edu.hse.ru>
2021-02-04 02:04:55 +0300

    Fix 3

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,23 +6,23 @@
 #include <gtest/gtest.h>
 #include <string>
 
-TEST (Deanary, test1) {
+TEST(Deanary, test1) {
     Student st("00000", "William Garcia");
     EXPECT_EQ("William Garcia", st.getFIO());
 }
 
-TEST (Deanary, test2) {
+TEST(Deanary, test2) {
     Student st("00000", "William Garcia");
     EXPECT_EQ("00000", st.getId());
 }
 
-TEST (Deanary, test3) {
+TEST(Deanary, test3) {
     Student st("00000", "William Garcia");
     st.addMark(5);
     EXPECT_EQ(5.0, st.getAverageMark());
 }
 
-TEST (Deanary, test4) {
+TEST(Deanary, test4) {
     Student st("00000", "William Garcia");
     Group gr("PI", "software engineering");
     st.addToGroup(&gr);
@@ -30,7 +30,7 @@ TEST (Deanary, test4) {
     EXPECT_EQ(5.0, gr.getStudents()[0]->getAverageMark());
 }
 
-TEST (Deanary, test5) {
+TEST(Deanary, test5) {
     Student st("00000", "William Garcia");
     Group gr("PI", "software engineering");
     gr.addStudent(&st);
@@ -38,21 +38,21 @@ TEST (Deanary, test5) {
     EXPECT_EQ("00000", gr.getHead()->getId());
 }
 
-TEST (Deanary, test6) {
+TEST(Deanary, test6) {
     Student st("00000", "William Garcia");
     Group gr("PI", "software engineering");
     gr.addStudent(&st);
     EXPECT_EQ(true, gr.containsStudent(&st));
 }
 
-TEST (Deanary, test7) {
+TEST(Deanary, test7) {
     Group gr("PI", "software engineering");
     Student st("00000", "William Garcia");
     gr.addStudent(&st);
     EXPECT_EQ(false, gr.isEmpty());
 }
 
-TEST (Deanary, test8) {
+TEST(Deanary, test8) {
     Group gr("PI", "software engineering");
     Student st("00000", "William Garcia");
     gr.addStudent(&st);
@@ -60,13 +60,13 @@ TEST (Deanary, test8) {
     EXPECT_EQ(true, gr.isEmpty());
 }
 
-TEST (Deanary, test9) {
+TEST(Deanary, test9) {
     Student st("00000", "William Garcia");
     st.setFIO("William White");
     EXPECT_EQ("William White", st.getFIO());
 }
 
-TEST (Deanary, test10) {
+TEST(Deanary, test10) {
     Group gr("PI", "software engineering");
     Student st("00000", "William Garcia");
     gr.addStudent(&st);
