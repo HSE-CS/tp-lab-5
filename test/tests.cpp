64718fd08baad041971559fe00170b3d32cb1fad
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2021-02-03 12:48:12 +0300

    test student 1,2

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,23 @@
-// Copyright 2020 GHA Test Team
+// Copyright 2021 Schenikova
 
 #include <gtest/gtest.h>
 #include <string>
+#include "Group.h"
+#include "Student.h"
+#include "Deanary.h"
 
+TEST(StudentTest, test1) {
+  unsigned int id = 12345678;
+  std::string fio = "Veselov Ignatiy Lvovich";
+  Student student(id, fio);
+  EXPECT_EQ(12345678, student.getId());
+}
+
+TEST(StudentTest, test2) {
+  unsigned int id = 98765432;
+  std::string fio = "Naumov Velor Iosifovich";
+  Student student(id, fio);
+  student.addMark(10);
+  EXPECT_EQ(10.0, student.getAveragemark());
+}
 
