ce846be72930abd798a71234e7c38d5eaaafba24
GlebNeshchetkin <glebneshchetkin@gmail.com>
2021-02-04 17:29:53 +0300

    FirstTest

--- src/Deanary.cpp
+++ src/Deanary.cpp
@@ -1,8 +1,7 @@
 // Copyright 2021 GN
 #include <string>
-#include <stdlib.h>
 #include <iostream>
-#include "Deanary.h"
+#include <Deanary.h>
 #include <fstream>
 void Deanary::acceptStudents() {
     std::string s;
@@ -27,7 +26,7 @@ void Deanary::createGroups() {
 void Deanary::studentRegistration() {
     srand(time(NULL));
     for (int i = 0; i < allStudents.size(); i++) {
-        int index = rand_r() % groups.size() + 0;
+        int index = rand() % groups.size() + 0;
         groups[index].addStudent(&allStudents[i]);
     }
 }
@@ -36,7 +35,7 @@ void Deanary::addMarks() {
     for (int gr_i = 0; gr_i < groups.size(); gr_i++) {
         for (int st_i = 0; st_i < groups[gr_i].students.size(); st_i++) {
             for (int i = 0; i < 10; i++) {
-                groups[gr_i].students[st_i]->addMark(rand_r() % 10 + 1);
+                groups[gr_i].students[st_i]->addMark(rand() % 10 + 1);
             }
         }
     }
