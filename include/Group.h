e104577456d7c1e9c0ed81ae070e1dce28b25c3a
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-02-05 16:32:53 +0300

    Add files via upload

---
+++ include/Group.h
@@ -0,0 +1,36 @@
+//  Copyright 2020 Baklanov
+
+#ifndef INCLUDE_Group_
+#define INCLUDE_Group_
+
+#include "Student.h"
+#include "Deanary.h"
+#include <iostream>
+#include <vector>
+
+class Student;
+
+class Group {
+private:
+	std::string title;
+	std::string spec;
+	std::vector<Student*> students;
+	Student* head {};
+	int acadPerform;
+	void addStudent(Student* s);
+	void removeStudent(Student* a);
+	void chooseHead(int number);
+public:
+	void chooseHeadTest(int number);
+	double getAveragemark();
+	void AddStudent(Student* s);
+	Student* getStudent(std::string student);
+	bool containsStudent(std::string student);
+	Student* getHead() {
+		return head;
+	}
+	bool isEmpty();
+	friend class Deanary;
+};
+
+#endif
---
+++ include/Student.h
@@ -0,0 +1,34 @@
+//  Copyright 2020 Baklanov
+#ifndef INCLUDE_Student_
+#define INCLUDE_Student_
+
+#include "Group.h"
+#include "Deanary.h"
+#include <iostream>
+#include <vector>
+
+class Group;
+
+class Student {
+private:
+	int id = -1;
+	std::string fio;
+	Group *group;
+	std::vector<int> marks;
+	std::string intel = "";
+	void addToGroup(Group& A);
+	void addmark(int mark);
+	bool isHead = 0;
+public:
+	Student(int num, std::string fi, Group* gr, int intellect);
+	Student(int num, std::string fi);
+	double getAvearagemark();
+	bool isHeadOfGroup();
+	int getId() {
+		return id;
+	}
+	friend class Deanary;
+	friend class Group;
+};
+
+#endif
