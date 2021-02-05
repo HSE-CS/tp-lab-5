84bc074a1c8da311f9472390cf4ca643b4667eba
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-02-05 17:30:23 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,4 @@
-
+//  Copyright 2020 Baklanov
 
 #include <gtest/gtest.h>
 #include <string>
@@ -6,83 +6,83 @@
 #include "../include/Group.h"
 
 TEST(Student, TEST1) {
-	Student student(1, "Ivanov Ivan Ivanovich");
-	EXPECT_EQ(1, student.getId());
+Student student(1, "Ivanov Ivan Ivanovich");
+EXPECT_EQ(1, student.getId());
 }
 
 TEST(Student, TEST2) {
-	Student student(1, "Ivanov Ivan Ivanovich");
-	EXPECT_EQ(false, student.isHeadOfGroup());
+Student student(1, "Ivanov Ivan Ivanovich");
+EXPECT_EQ(false, student.isHeadOfGroup());
 }
 
 TEST(Student, TEST3) {
-	Student student(1, "Ivanov Ivan Ivanovich");
-	student.addmark(5);
-	student.addmark(7);
-	EXPECT_EQ(6, student.getAvearagemark());
+Student student(1, "Ivanov Ivan Ivanovich");
+student.addmark(5);
+student.addmark(7);
+EXPECT_EQ(6, student.getAvearagemark());
 }
 
 TEST(Group, TEST4) {
-	Group gr;
-	auto* student = new Student(1, "Ivanov Ivan Ivanovich");
-	gr.AddStudent(student);
-	EXPECT_EQ("Ivanov Ivan Ivanovich", gr.getStudent("1")->getFio());
+Group gr;
+auto* student = new Student(1, "Ivanov Ivan Ivanovich");
+gr.AddStudent(student);
+EXPECT_EQ("Ivanov Ivan Ivanovich", gr.getStudent("1")->getFio());
 }
 
 TEST(Group, TEST5) {
-	Group gr;
-	auto* student = new Student(1, "Ivanov Ivan Ivanovich");
-	gr.AddStudent(student);
-	gr.chooseHeadTest(0);
-	EXPECT_EQ(true, gr.getStudent("1")->isHeadOfGroup());
+Group gr;
+auto* student = new Student(1, "Ivanov Ivan Ivanovich");
+gr.AddStudent(student);
+gr.chooseHeadTest(0);
+EXPECT_EQ(true, gr.getStudent("1")->isHeadOfGroup());
 }
 
 TEST(Group, TEST6) {
-	Group gr;
-	auto* student = new Student(1, "Ivanov Ivan Ivanovich");
-	gr.AddStudent(student);
-	gr.chooseHeadTest(0);
-	EXPECT_EQ("Ivanov Ivan Ivanovich", gr.getHead()->getFio());
+Group gr;
+auto* student = new Student(1, "Ivanov Ivan Ivanovich");
+gr.AddStudent(student);
+gr.chooseHeadTest(0);
+EXPECT_EQ("Ivanov Ivan Ivanovich", gr.getHead()->getFio());
 }
 
 TEST(Group, TEST7) {
-	Group gr;
-	auto* student = new Student(1, "Ivanov Ivan Ivanovich");
-	auto* student2 = new Student(2, "Petrov Petr Petrovich");
-	gr.AddStudent(student);
-	gr.AddStudent(student2);
-	EXPECT_EQ(0, gr.getAveragemark());
+Group gr;
+auto* student = new Student(1, "Ivanov Ivan Ivanovich");
+auto* student2 = new Student(2, "Petrov Petr Petrovich");
+gr.AddStudent(student);
+gr.AddStudent(student2);
+EXPECT_EQ(0, gr.getAveragemark());
 }
 
 TEST(Group, TEST8) {
-	Group gr;
-	auto* student = new Student(1, "Ivanov Ivan Ivanovich");
-	auto* student2 = new Student(2, "Petrov Petr Petrovich");
-	gr.AddStudent(student);
-	gr.AddStudent(student2);
-	gr.getStudent("1")->addmark(1);
-	gr.getStudent("1")->addmark(3);
-	gr.getStudent("1")->addmark(5);
-	gr.getStudent("2")->addmark(1);
-	gr.getStudent("2")->addmark(3);
-	gr.getStudent("2")->addmark(5);
-	EXPECT_EQ(3, gr.getAveragemark());
+Group gr;
+auto* student = new Student(1, "Ivanov Ivan Ivanovich");
+auto* student2 = new Student(2, "Petrov Petr Petrovich");
+gr.AddStudent(student);
+gr.AddStudent(student2);
+gr.getStudent("1")->addmark(1);
+gr.getStudent("1")->addmark(3);
+gr.getStudent("1")->addmark(5);
+gr.getStudent("2")->addmark(1);
+gr.getStudent("2")->addmark(3);
+gr.getStudent("2")->addmark(5);
+EXPECT_EQ(3, gr.getAveragemark());
 }
 
 
 TEST(Student, TEST9) {
-	Deanary dec;
-	dec.createGroups("Groups.txt");
-	dec.hireStudents("Students.txt");
-	EXPECT_EQ(0, dec.getStatistics());
+Deanary dec;
+dec.createGroups("Groups.txt");
+dec.hireStudents("Students.txt");
+EXPECT_EQ(0, dec.getStatistics());
 }
 
 TEST(Group, TEST10) {
-	Group gr;
-	auto* student = new Student(1, "Ivanov Ivan Ivanovich");
-	auto* student2 = new Student(2, "Petrov Petr Petrovich");
-	gr.AddStudent(student);
-	gr.AddStudent(student2);
-	gr.chooseHeadTest(1);
-	EXPECT_EQ("Petrov Petr Petrovich", gr.getHead()->getFio());
+Group gr;
+auto* student = new Student(1, "Ivanov Ivan Ivanovich");
+auto* student2 = new Student(2, "Petrov Petr Petrovich");
+gr.AddStudent(student);
+gr.AddStudent(student2);
+gr.chooseHeadTest(1);
+EXPECT_EQ("Petrov Petr Petrovich", gr.getHead()->getFio());
 }
