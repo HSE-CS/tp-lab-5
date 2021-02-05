143c4de9cfd207287aa507319cf126eb0794ca62
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-02-05 22:05:36 +0300

    Update Student.h

--- include/Student.h
+++ include/Student.h
@@ -13,28 +13,28 @@ class Group;
 
 class Student {
  private:
-int id = -1;
-std::string fio;
-Group *group;
-std::vector<int> marks;
-std::string intel = "";
-void addToGroup(Group& A);
-bool isHead = 0;
+  int id = -1;
+  std::string fio;
+  Group *group;
+  std::vector<int> marks;
+  std::string intel = "";
+  void addToGroup(Group* A);
+  bool isHead = 0;
 
  public:
-Student(int num, std::string fi, Group* gr, int intellect);
-Student(int num, std::string fi);
-double getAvearagemark();
-void addmark(int mark);
-std::string getFio() {
-return fio;
-}
-bool isHeadOfGroup();
-int getId() {
-return id;
-}
-friend class Deanary;
-friend class Group;
+  Student(int num, std::string fi, Group* gr, int intellect);
+  Student(int num, std::string fi);
+  double getAvearagemark();
+  void addmark(int mark);
+  std::string getFio() {
+  return fio;
+  }
+  bool isHeadOfGroup();
+  int getId() {
+  return id;
+  }
+  friend class Deanary;
+  friend class Group;
 };
 
 #endif  // INCLUDE_STUDENT_H_
