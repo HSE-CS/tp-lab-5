e1b3d736bfc12237d12295347c189954bca43e9f
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-02-05 15:32:24 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "Deanary.h"
+#include "Student.h"
+#include "Group.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <locale.h>  
+#include <cstring> 
+
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "Russian");
+	system("chcp 1251");
+	std::ofstream fout;
+	fout.open("Groupmarks.txt");
+	fout << "HEY" << "\n";
+	Deanary dec;
+	std::string c = "check";
+	std::ifstream fin;
+	dec.createGroups("Groups.txt");
+	dec.hireStudents("Students.txt");
+	dec.addMarksToAll();
+	dec.getStatistics();
+	dec.saveStaff("Groups.txt", "Students.txt");
+}
