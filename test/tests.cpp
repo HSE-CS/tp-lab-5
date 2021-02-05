1e82578cf8285448952856eb6938d63544f090af
natasha <igum.natasha@gmail.com>
2021-02-05 22:51:21 +0300

    Fix bug

--- test/tests.cpp
+++ test/tests.cpp
@@ -26,31 +26,31 @@ TEST(TestDeanary, Test3) {
 }
 
 TEST(TestDeanary, Test4) {
-    Deanary dr(3, "../Students/data.txt");
-    EXPECT_EQ("Oleg Komarov", dr.findSudentId(201)->getFio());
+    Deanary dr(3, "data.txt");
+    EXPECT_STREQ("Oleg Komarov", dr.findSudentId(201)->getFio());
 }
 
 TEST(TestDeanary, Test5) {
-    Deanary dr(3, "../Students/data.txt");
+    Deanary dr(3, "data.txt");
     Student* st = dr.findGroupTitle("19_1")->getStudent(108);
     dr.moveStudents(108, "19_3");
     EXPECT_EQ(false, dr.findGroupTitle("19_1")->containsStudents(st));
 }
 
 TEST(TestDeanary, Test6) {
-    Deanary dr(3, "../Students/data.txt");
+    Deanary dr(3, "data.txt");
     Student* st = dr.findGroupTitle("19_1")->getStudent(108);
     dr.moveStudents(108, "19_3");
     EXPECT_EQ(true, dr.findGroupTitle("19_3")->containsStudents(st));
 }
 
 TEST(TestDeanary, Test7) {
-	Deanary dr(3, "../Students/data.txt");
+	Deanary dr(3, "data.txt");
 	EXPECT_EQ(5.0, dr.findSudentId(201)->getAveragemark());
 }
 
 TEST(TestDeanary, Test8) {
-    Deanary dr(3, "../Students/data.txt");
+    Deanary dr(3, "data.txt");
     EXPECT_EQ(false, dr.findGroupTitle("19_1")->isEmpty());
 }
 
