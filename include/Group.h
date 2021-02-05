1f1c461b2730542a6710957d115e3edeb3eb1656
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-02-05 17:55:20 +0300

    Update Group.h

--- include/Group.h
+++ include/Group.h
@@ -1,36 +1,38 @@
 //  Copyright 2020 Baklanov
 
-#ifndef INCLUDE_Group_
-#define INCLUDE_Group_
+#ifndef INCLUDE_GROUP_H_
+#define INCLUDE_GROUP_H_
 
 #include "Student.h"
 #include "Deanary.h"
 #include <iostream>
 #include <vector>
+#include <string>
 
 class Student;
 
 class Group {
-private:
-	std::string title;
-	std::string spec;
-	std::vector<Student*> students;
-	Student* head {};
-	int acadPerform;
-	void addStudent(Student* s);
-	void removeStudent(Student* a);
-	void chooseHead(int number);
-public:
-	void chooseHeadTest(int number);
-	double getAveragemark();
-	void AddStudent(Student* s);
-	Student* getStudent(std::string student);
-	bool containsStudent(std::string student);
-	Student* getHead() {
-		return head;
-	}
-	bool isEmpty();
-	friend class Deanary;
+ private:
+std::string title;
+std::string spec;
+std::vector<Student*> students;
+Student* head {};
+int acadPerform;
+void addStudent(Student* s);
+void removeStudent(Student* a);
+void chooseHead(int number);
+	
+ public:
+void chooseHeadTest(int number);
+double getAveragemark();
+void AddStudent(Student* s);
+Student* getStudent(std::string student);
+bool containsStudent(std::string student);
+Student* getHead() {
+return head;
+}
+bool isEmpty();
+friend class Deanary;
 };
 
-#endif
+#endif // INCLUDE_GROUP_H_
