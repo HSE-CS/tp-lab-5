f3ace4ec7333b41c3b87911d24cadc9070cfe3e2
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-02-05 17:39:16 +0300

    Update Student.cpp

--- src/Student.cpp
+++ src/Student.cpp
@@ -1,53 +1,54 @@
+//  Copyright 2020 Baklanov
+
 #include "Student.h"
 #include "Group.h"
 
 void Student::addmark(int mark) {
-	if (mark < 4 and intel == "high") {
-			mark += 6;
-	}
-	else if (mark >= 5 and intel == "low") {
-		mark -= 4;
-	}
-	marks.push_back(mark);
+if ((mark < 4) && (intel == "high")) {
+mark += 6;
+} else if ((mark >= 5) && (intel == "low")) {
+mark -= 4;
+}
+marks.push_back(mark);
 }
 
 void Student::addToGroup(Group& A) {
-	*group = A;
+*group = A;
 }
 
 double Student::getAvearagemark() {
-	double sum = 0;
-	int mount = this->marks.size();
-	for (int i = 0; i < mount; ++i) {
-		sum += this->marks[i];
-	}
-	if (mount == 0) {
-		return 0;
-	}
-	return sum / mount;
+double sum = 0;
+int mount = this->marks.size();
+for (int i = 0; i < mount; ++i) {
+sum += this->marks[i];
+}
+if (mount == 0) {
+return 0;
+}
+return sum / mount;
 }
 bool Student::isHeadOfGroup() {
-	return this->isHead;
+return this->isHead;
 }
 
 Student::Student(int num, std::string fi, Group* gr, int intellegence) {
-	switch (intellegence) {
-	case (1):
-		intel = "low";
-		break;
-	case(2,3):
-		intel = "average";
-		break;
-	case(4):
-		intel = "high";
-		break;
-	}
-	id = num;
-	fio = fi;
-	group = gr;
+switch (intellegence) {
+case (1):
+intel = "low";
+break;
+case(2, 3):
+intel = "average";
+break;
+case(4):
+intel = "high";
+break;
+}
+id = num;
+fio = fi;
+group = gr;
 }
 
 Student::Student(int num, std::string fi) {
-	id = num;
-	fio = fi;
+id = num;
+fio = fi;
 }
