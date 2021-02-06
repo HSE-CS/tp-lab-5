b47af5e8215f66b9558c15c080e01311352133f6
Андрей Рогов <adrogov@edu.hse.ru>
2021-02-06 19:09:47 +0300

    sec

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,70 +3,72 @@
 #include "../include/Student.h"
 #include "../include/Group.h"
 #include "../include/Deanary.h"
+#include <gtest/gtest.h>
+#include <string>
 
 TEST(Deanary, test1) {
-    Student st("00000", "William Garcia");
+    Student st("00000", "Donald Trump");
     EXPECT_EQ("William Garcia", st.getFIO());
 }
 
 TEST(Deanary, test2) {
-    Student st("00000", "William Garcia");
+    Student st("00000", "Donald Trump");
     EXPECT_EQ("00000", st.getId());
 }
 
 TEST(Deanary, test3) {
-    Student st("00000", "William Garcia");
+    Student st("00000", "Donald Trump");
     st.addMark(5);
     EXPECT_EQ(5.0, st.getAverageMark());
 }
 
 TEST(Deanary, test4) {
-    Student st("00000", "William Garcia");
-    Group gr("PI", "software engineering");
+    Student st("00000", "Donald Trump");
+    Group gr("PI", "hardware engineering");
     st.addToGroup(&gr);
     gr.getStudents()[0]->addMark(5);
     EXPECT_EQ(5.0, gr.getStudents()[0]->getAverageMark());
 }
 
 TEST(Deanary, test5) {
-    Student st("00000", "William Garcia");
-    Group gr("PI", "software engineering");
+    Student st("00000", "Donald Trump");
+    Group gr("PI", "hardware engineering");
     gr.addStudent(&st);
     gr.chooseHead(&st);
     EXPECT_EQ("00000", gr.getHead()->getId());
 }
 
 TEST(Deanary, test6) {
-    Student st("00000", "William Garcia");
-    Group gr("PI", "software engineering");
+    Student st("00000", "Donald Trump");
+    Group gr("PI", "hardware engineering");
     gr.addStudent(&st);
     EXPECT_EQ(true, gr.containsStudent(&st));
 }
 
 TEST(Deanary, test7) {
-    Group gr("PI", "software engineering");
-    Student st("00000", "William Garcia");
+    Group gr("PI", "hardware engineering");
+    Student st("00000", "Donald Trump");
     gr.addStudent(&st);
     EXPECT_EQ(false, gr.isEmpty());
 }
 
 TEST(Deanary, test8) {
-    Group gr("PI", "software engineering");
-    Student st("00000", "William Garcia");
+    Group gr("PI", "hardware engineering");
+    Student st("00000", "Donald Trump");
     gr.addStudent(&st);
     gr.removeStudent(&st);
     EXPECT_EQ(true, gr.isEmpty());
 }
 
 TEST(Deanary, test9) {
-    Student st("00000", "William Garcia");
-    st.setFIO("William White");
-    EXPECT_EQ("William White", st.getFIO());
+    Student st("00000", "Donald Trump");
+    st.setFIO("Donald Mcduck");
+    EXPECT_EQ("Donald Mcduck", st.getFIO());
 }
 
 TEST(Deanary, test10) {
-    Group gr("PI", "software engineering");
-    Student st("00000", "William Garcia");
+    Group gr("PI", "hardware engineering");
+    Student st("00000", "Donald Trump");
     gr.addStudent(&st);
-    EXPECT_EQ("software engineering", st.getGr()->getSpec());
+    EXPECT_EQ("hardware engineering", st.getGr()->getSpec());
 }
