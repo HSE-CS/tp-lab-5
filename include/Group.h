fb6625fd63053590d601be32bdc40cd66da60052
Андрей Рогов <adrogov@edu.hse.ru>
2021-02-06 19:44:56 +0300

    spaces..

--- include/Group.h
+++ include/Group.h
@@ -10,38 +10,25 @@
 class Student;
 
 class Group {
- private:
-     std::string title;
-     std::string spec;
-     std::vector<Student*> students;
-     Student* head{ nullptr };
- 
- public:
-     Group(const std::string& t, const std::string& s);
- 
-     std::string getTitle();
- 
-     std::string getSpec();
- 
-     void addStudent(Student* s);
- 
-     void chooseHead(Student* h);
- 
-     Student* getHead();
- 
-     double getAverageMark();
- 
-     Student* getStudent(const std::string& sid);
- 
-     bool containsStudent(Student* s);
- 
-     void removeStudent(Student* s);
- 
-     bool isEmpty();
- 
-     std::vector<Student*> getStudents();
- 
-     ~Group();
+private:
+    std::string title;
+    std::string spec;
+    std::vector<Student*> students;
+    Student* head{ nullptr };
+public:
+    Group(const std::string& t, const std::string& s);
+    std::string getTitle();
+    std::string getSpec();
+    void addStudent(Student* s);
+    void chooseHead(Student* h);
+    Student* getHead();
+    double getAverageMark();
+    Student* getStudent(const std::string& sid);
+    bool containsStudent(Student* s);
+    void removeStudent(Student* s);
+    bool isEmpty();
+    std::vector<Student*> getStudents();
+    ~Group();
 };
 
 #endif  // INCLUDE_GROUP_H_
--- include/Student.h
+++ include/Student.h
@@ -10,36 +10,24 @@
 class Group;
 
 class Student {
- private:
-     std::string fio;
-     std::string id;
-     std::vector<int> marks;
-     Group* group{};
- 
- public:
-     Student(const std::string& i, const std::string& f);
- 
-     void addToGroup(Group* g);
- 
-     void addMark(int mark);
- 
-     double getAverageMark();
- 
-     void setFIO(std::string f);
- 
-     std::string getFIO();
- 
-     void setId(std::string i);
- 
-     std::string getId();
- 
-     void setGr(Group* g);
- 
-     Group* getGr() const;
- 
-     std::vector<int> getMarks() const;
- 
-     ~Student();
+private:
+    std::string fio;
+    std::string id;
+    std::vector<int> marks;
+    Group* group{};
+public:
+    Student(const std::string& i, const std::string& f);
+    void addToGroup(Group* g);
+    void addMark(int mark);
+    double getAverageMark();
+    void setFIO(std::string f);
+    std::string getFIO();
+    void setId(std::string i);
+    std::string getId();
+    void setGr(Group* g);
+    Group* getGr() const;
+    std::vector<int> getMarks() const;
+    ~Student();
 };
 
 #endif  // INCLUDE_STUDENT_H_
