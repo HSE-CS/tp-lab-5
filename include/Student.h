4e671a305be1f381f53a50326f57f57f94596b41
TimurZaytsev <tz200128@gmail.com>
2021-02-07 22:40:30 +0300

    seventh

--- include/Student.h
+++ include/Student.h
@@ -9,20 +9,19 @@
 class Group;
 
 class Student {
-public:
-	double get_average_mark() const;
-	bool is_head() const;
-	friend class Group;
-	friend class Deanary;
+ public:
+  double get_average_mark() const;
+  bool is_head() const;
+  friend class Group;
+  friend class Deanary;
 
-private:
-	Student(uint64_t, const std::string&, Group* = nullptr);
-	void add_mark(int);
-
-	std::string name;
-	uint64_t id;
-	Group* group;
-	std::vector<int> marks;
+ private:
+  Student(uint64_t, const std::string&, Group* = nullptr);
+  void add_mark(int);
+  std::string name;
+  uint64_t id;
+  Group* group;
+  std::vector<int> marks;
 };
 
 #endif  // INCLUDE_STUDENT_H_
