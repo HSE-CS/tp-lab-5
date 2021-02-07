59b1901d1ee84377ec9aed83dc3623f374ab3d11
Anton Klykov <antonklykov01@mail.ru>
2021-02-07 16:11:14 +0300

    Change the tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -13,39 +13,46 @@ TEST(TestingDeanary, test1) {
   EXPECT_EQ(a, b);
 }
 TEST(TestingDeanary, test2) {
+  Deanary deanary;
   deanary.createGroups();
   int a = 3;
   int b = deanary.getAmountGroup();
   EXPECT_EQ(a, b);
 }
 TEST(TestingDeanary, test3) {
+  Deanary deanary;
   std::string a = "15PMI-2";
   int p = 1;
   std::string b = deanary.getTitleGroup(p);
   EXPECT_EQ(a, b);
 }
 TEST(TastingDeanary, test4) {
+  Deanary deanary;
   int a = 0;
   int b = deanary.getAmountStudents(0);
   EXPECT_EQ(a, b);
 }
 TEST(TestingDeanary, test5) {
+  Deanary deanary;
   deanary.hireStudent();
   int a = deanary.getAmountStudents(0);
   EXPECT_EQ(a, deanary.getAmountStudents(0));
 }
 TEST(TestingDeanary, test6) {
+  Deanary deanary;
   bool a = false;
   bool b = deanary.getGroup(1);
   EXPECT_EQ(a, b);
 }
 TEST(TestingDeanary, test7) {
+  Deanary deanary;
   Group* a = deanary.getGroup(0);
   Student* b = a->getStudent(3);
   bool c = false;
   EXPECT_EQ(c, b->isHeadOfGroup());
 }
 TEST(TestingDeanary, test8) {
+  Deanary deanary;
   Group* a = deanary.getGroup(0);
   deanary.initHeaders();
   Student* b = a->getHeader();
@@ -53,17 +60,20 @@ TEST(TestingDeanary, test8) {
   EXPECT_EQ(c, b->isHeadOfGroup());
 }
 TEST(TestingDeanary, test9) {
+  Deanary deanary;
   Group* a = deanary.getGroup(0);
   Student* b = a->getStudent(4);
   EXPECT_EQ(b->getGroup(), a);
 }
 TEST(TestingDeanary, test10) {
+  Deanary deanary;
   Group* a = deanary.getGroup(0);
   Student* b = a->getStudent(4);
   int c = 0;
   EXPECT_EQ(c, b->getAverageMark());
 }
 TEST(TestingDeanary, test11) {
+  Deanary deanary;
   deanary.addMarksToAll();
   deanary.addMarksToAll();
   deanary.addMarksToAll();
@@ -73,6 +83,7 @@ TEST(TestingDeanary, test11) {
   EXPECT_EQ(c, b->getAverageMark());
 }
 TEST(TestingDeanary, test12) {
+  Deanary deanary;
   deanary.addMarksToAll();
   deanary.addMarksToAll();
   deanary.addMarksToAll();
@@ -82,15 +93,14 @@ TEST(TestingDeanary, test12) {
   EXPECT_EQ(c, b->getAmountMarks());
 }
 TEST(TestingDeanary, test13) {
-  deanary.addMarksToAll();
-  deanary.addMarksToAll();
-  deanary.addMarksToAll();
+  Deanary deanary;
   int size1 = deanary.getAmountStudents(0);
   deanary.fireStudent(0);
   int size2 = deanary.getAmountStudents(0);
   EXPECT_EQ(size1 - 1, size2);
 }
 TEST(TestingDeanary, test14) {
+  Deanary deanary;
   int size1 = deanary.getAmountStudents(1);
   deanary.moveStudent(0, 1);
   int size2 = deanary.getAmountStudents(1);
