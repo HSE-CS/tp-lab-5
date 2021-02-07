ecebb330453ff64266b70e2cd1143fdd57fb0df9
NoName <ihackit595@gmail.com>
2021-02-07 22:22:21 +0300

    complete tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -41,30 +41,54 @@ TEST(AllTests, test5) {
 }
 
 TEST(AllTests, test6) {
-    EXPECT_EQ(18.0, 18);
+    Group *gr = new Group("19PMI-1", "Computer Science");
+    EXPECT_EQ(gr->getTitle(), "19PMI-1");
+    EXPECT_EQ(gr->getSpec(), "Computer Science");
 }
 
 TEST(AllTests, test7) {
-    EXPECT_EQ(18.0, 18);
+    Group *gr = new Group("19PMI-1", "Computer Science");
+    EXPECT_EQ(gr->isEmpty(), 1);
 }
 
 TEST(AllTests, test8) {
-    EXPECT_EQ(18.0, 18);
+    Student *st = new Student(1, "Student A");
+    Group *gr = new Group("19PMI-1", "Computer Science");
+    gr->addStudentToGroup(st);
+    std::string fio = "Student A";
+    EXPECT_EQ(gr->findStudent(fio), st);
 }
 
-
 TEST(AllTests, test9) {
-    EXPECT_EQ(18.0, 18);
+    Student *st = new Student(1, "Student A");
+    Student *sf = new Student(2, "Student B");
+    st->addMark(2);
+    sf->addMark(10);
+    Group *gr = new Group("19PMI-1", "Computer Science");
+    gr->addStudentToGroup(st);
+    gr->addStudentToGroup(sf);
+    EXPECT_EQ(gr->calculateMeanMark(), 6);
 }
 
 TEST(AllTests, test10) {
-    EXPECT_EQ(18.0, 18);
+    Deanery dn = Deanery();
+    Student *st = new Student(1, "Student A");
+    Student *sf = new Student(2, "Student B");
+    st->addMark(2);
+    sf->addMark(10);
+    Group *gr = new Group("19PMI-1", "Computer Science");
+    gr->addStudentToGroup(st);
+    gr->addStudentToGroup(sf);
+    std::vector<Group*> groups;
+    groups.push_back(gr);
+    dn.setGroups(groups);
+    std::vector<float> stats = dn.getStatistics();
+    EXPECT_EQ(stats[0], 6);
 }
 
 TEST(AllTests, test11) {
-    EXPECT_EQ(18.0, 18);
+    Deanery dn = Deanery();
+    std::vector<Group*> groups = dn.createGroupsFromFile("../src/input.json");
+    EXPECT_EQ(groups[0]->getStudents()[0]->getFio(), "Marcia Herring");
 }
 
-TEST(AllTests, test12) {
-    EXPECT_EQ(18.0, 18);
-}
