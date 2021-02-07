9a781238eeb4ad462fbaf0ba9367582686382982
TimurZaytsev <tz200128@gmail.com>
2021-02-07 22:51:25 +0300

    tenth

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@
 #include "Group.h"
 
 int main() {
-	Deanary d;
-	d.from_file("students.txt");
-	d.add_marks_to_all(2);
-	d.add_marks_to_all(5);
-	d.add_marks_to_all(5);
-	std::cout << d.get_statistics();
+  Deanary d;
+  d.from_file("students.txt");
+  d.add_marks_to_all(2);
+  d.add_marks_to_all(5);
+  d.add_marks_to_all(5);
+  std::cout << d.get_statistics();
 
-	return 0;
+  return 0;
 }
