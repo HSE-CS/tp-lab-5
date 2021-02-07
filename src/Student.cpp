d5552aa50b6cca3b4e634585f79109bb0c0f5286
GDM1705 <71228914+GDM1705@users.noreply.github.com>
2021-02-07 22:48:11 +0300

    Update Student.cpp

--- src/Student.cpp
+++ src/Student.cpp
@@ -10,53 +10,53 @@ void addMark();
 double calculateAverageGrade();
 
 Student::Student(std::string id, std::string fio)
-	: id(std::move(id)), fio(std::move(fio)) {}
+: id(std::move(id)), fio(std::move(fio)) {}
 
 void Student::addToGroup(Group* group) {
-	this->group = group;
-	if (group->getStudent(this->id) == nullptr) {
-		group->addStudent(this);
-	}
+this->group = group;
+if (group->getStudent(this->id) == nullptr) {
+group->addStudent(this);
+}
 }
 
 Group* Student::getGroup() const {
-	return group;
+return group;
 }
 
 std::string Student::getId() const {
-	return id;
+return id;
 }
 const std::string& Student::getFio() const {
-	return fio;
+return fio;
 }
 void Student::setId(std::string id) {
-	Student::id = id;
+Student::id = id;
 }
 void Student::setFio(const std::string& fio) {
-	Student::fio = fio;
+Student::fio = fio;
 }
 
 void Student::addMark(int mark) {
-	this->marks.reserve(this->marks.size() + 1);
-	this->marks.push_back(mark);
+this->marks.reserve(this->marks.size() + 1);
+this->marks.push_back(mark);
 }
 
 double Student::calculateAverageMark() {
-	double sum = 0;
-	int counter = 0;
-	for (int i = 0; i < this->marks.size(); ++i) {
-		sum += this->marks.at(i);
-		counter++;
-	}
-	return sum / counter;
+double sum = 0;
+int counter = 0;
+for (int i = 0; i < this->marks.size(); ++i) {
+sum += this->marks.at(i);
+counter++;
+}
+return sum / counter;
 }
 
 void Student::setGroup(Group* group) {
-	Student::group = group;
+Student::group = group;
 }
 
 const std::vector<int>& Student::getMarks() const {
-	return marks;
+return marks;
 }
 
 Student::~Student() = default;
