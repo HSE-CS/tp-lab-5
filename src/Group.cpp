cb274c4d68883963b92f29204ea996298b0d408b
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2021-02-07 22:52:08 +0300

    initial commit

---
+++ src/Group.cpp
@@ -0,0 +1,86 @@
+// Copyright 2021 ArinaMonicheva
+
+#include "Group.h"
+
+Group::Group(std::string newTitle = "") {
+  title = newTitle;
+  head = NULL;
+}
+
+void Group::addStudent(Student& newStudent) {
+  if (containsStudent(newStudent.id) == -1) {
+    students.push_back(&newStudent);
+    newStudent.group = this;
+  }
+}
+
+void Group::chooseHead(Student* candidate) {
+  if (containsStudent(candidate->id) > -1) {
+    head = candidate;
+    candidate->isHead = true;
+  }
+}
+
+Student& Group::getStudent(unsigned id) const {
+  for (auto i : students) {
+    if (i->id == id) {
+      return *i;
+    }
+  }
+  throw "No such student";
+} 
+
+Student& Group::getStudent(std::string fio) const {
+  for (auto i : students) {
+    if (i->fio == fio) {
+      return *i;
+    }
+  }
+  throw "No such student";
+}
+
+int Group::containsStudent(unsigned id) {
+  int j = 0;
+  for (auto i : students) {
+    if (i->id == id) {
+      return j;
+    }
+    j++;
+  }
+  return -1;
+}
+
+int Group::containsStudent(std::string fio) {
+  int j = 0;
+  for (auto i : students) {
+    if (i->fio == fio) {
+      return j;
+    }
+    j++;
+  }
+  return -1;
+}
+
+double Group::getAverageMark() {
+  double averageMark = 0.0;
+  int j = 0;
+
+  for (auto i : students) {
+    averageMark += i->getAverageMark();
+    j++;
+  }
+
+  return averageMark / j;
+}
+
+void Group::removeStudent(Student& removalOne) {
+  int index = containsStudent(removalOne.id);
+  if (index > -1) {
+    if (removalOne.isHeadOfGroup()) {
+      removalOne.isHead = false;
+      head = NULL;
+    }
+    students.erase(students.begin() + index);
+    removalOne.group = NULL;
+  }
+}
---
+++ src/main.cpp
@@ -0,0 +1,83 @@
+// Copyright 2021 ArinaMonicheva
+
+#include <iostream>
+#include "Deanary.h"
+
+int main() {
+  time_t* n = NULL;
+  srand(time(n));
+  Deanery newDeanery;
+  Group* newGroup = new Group("19PI-2");
+
+  std::ifstream groupsFile("groups.txt");
+  newDeanery.createGroups(groupsFile);
+  groupsFile.close();
+  
+  std::ifstream studentsFile("students.txt");
+  newDeanery.hireStudents(studentsFile);
+  studentsFile.close();
+
+  newDeanery.addMarksToAll();
+
+  std::vector<Group*> allGroups = newDeanery.getGroups();
+  newDeanery.printToConsole(*(allGroups[0]));
+  // changing the value of the first item in vector 
+  allGroups[0] = newGroup;
+  // printed result woulb be different
+  newDeanery.printToConsole(*(allGroups[0]));
+  allGroups = newDeanery.getGroups();
+  // but the original class object has not changed
+  newDeanery.printToConsole(*(allGroups[0]));
+
+  stats stats1 = newDeanery.getStatistics();
+  newDeanery.printToConsole(stats1);
+
+  Student* newStudent = new Student(123456, "Monicheva Arina Aleksandrovna");
+  for (int i = 0; i < 8; i++) {
+    newStudent->addMark(6 + rand() % 5);
+  }
+
+  std::cout << newStudent->getAverageMark() << std::endl;
+
+  allGroups[0]->addStudent(*newStudent);
+  stats1 = newDeanery.getStatistics();
+  newDeanery.printToConsole(stats1);
+
+  newDeanery.initHeads(*(allGroups[0]));
+  newDeanery.printToConsole(*(allGroups[0]));
+  std::cout << newStudent->isHeadOfGroup() << std::endl;
+
+  newDeanery.printToConsole(allGroups[0]->getStudent(123456));
+  newDeanery.printToConsole(
+  allGroups[0]->getStudent("Monicheva Arina Aleksandrovna"));
+  std::cout << "Num in list: " << allGroups[0]->containsStudent(123456)
+  << "Num in list: " << 
+  allGroups[0]->containsStudent("Monicheva Arina Aleksandrovna") << std::endl;
+
+  newDeanery.moveStudents(*newStudent, *newGroup);
+  newDeanery.printToConsole(*newStudent);
+
+  stats1 = newDeanery.getStatistics();
+  newDeanery.printToConsole(stats1);
+
+  newDeanery.addNewGroup(newGroup);
+  stats1 = newDeanery.getStatistics();
+  newDeanery.printToConsole(stats1);
+
+  newDeanery.fireStudents();
+  allGroups = newDeanery.getGroups();
+  for (int i = 0; i < allGroups.size(); i++) {
+    newDeanery.printToConsole(*(allGroups[i]));
+  }
+
+  std::ofstream resafeSt("restudents.txt");
+  std::ofstream resafeGr("regroups.txt");
+  newDeanery.safeStaff(resafeGr, resafeSt);
+  resafeGr.close();
+  resafeSt.close();
+
+  delete newGroup;
+  delete newStudent;
+
+  return 0;
+}
