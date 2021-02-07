f197258981612eabc27fe44055451351623d746e
Anton Klykov <antonklykov01@mail.ru>
2021-02-07 17:08:43 +0300

    Again

--- include/Deanary.h
+++ include/Deanary.h
@@ -12,6 +12,8 @@
 #include <fstream>
 #include "json.hpp"
 
+class Deanary;
+
 using json = nlohmann::json;
 class Group;
 class Deanary {
@@ -20,49 +22,52 @@ class Deanary {
   class CreateGroup_F {
    private:
     std::vector<std::string> title_vector;
+    Group* gr_p;
 
    public:
+    ~CreateGroup_F() {
+      delete gr_p;
+    }
     std::vector<std::string> getTitle() {
       return this->title_vector;
     }
     void readFile() {
-      std::ifstream fin("img/groups.json");
-      json j;
-      j = json::parse(fin);
+      std::ifstream fin("groups.json");
+      json j = json::parse(fin);
       j["title"].get_to<std::vector<std::string>>(this->title_vector);
       fin.close();
     }
-    Group* parseGroup() {
-      static int iter{0};
-      auto *gr_p = new Group(title_vector[iter]);
-      iter++;
+    Group* parseGroup(int i) {
+      gr_p = new Group(title_vector[i]);
       return gr_p;
     }
   };
   class CreateStudent_F {
    private:
     std::vector<std::string> fio_student;
+    Student* st_p;
 
    public:
+    ~CreateStudent_F() {
+      delete st_p;
+    }
     std::vector<std::string> getFio() {
       return this->fio_student;
     }
     void readFile() {
-      std::ifstream oin("img/students.json");
-      json j;
-      j = json::parse(oin);
+      std::ifstream oin("students.json");
+      json j = json::parse(oin);
       j["fio"].get_to<std::vector<std::string>>(this->fio_student);
       oin.close();
     }
-    Student* parseStudent() {
-      static int it{0};
-      auto* st_p = new Student(fio_student[it]);
-      it++;
+    Student* parseStudent(int i) {
+      st_p = new Student(fio_student[i]);
       return st_p;
     }
   };
 
  public:
+  ~Deanary();
   void createGroups();
   void hireStudent();
   void addMarksToAll();
@@ -78,3 +83,4 @@ class Deanary {
 };
 
 #endif  // INCLUDE_DEANARY_H_
+
