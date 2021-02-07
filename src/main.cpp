b54d54768773425ad2c6b6b518b9d1975e434f89
ArinaMonicheva <63364781+ArinaMonicheva@users.noreply.github.com>
2021-02-07 23:41:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 int main() {
   time_t* n = NULL;
+  unsigned* whyyy = 0;
   srand(time(n));
   Deanery newDeanery;
   Group* newGroup = new Group("19PI-2");
@@ -20,31 +21,31 @@ int main() {
   newDeanery.addMarksToAll();
 
   std::vector<Group*> allGroups = newDeanery.getGroups();
-  newDeanery.printToConsole(*(allGroups[0]));
+  newDeanery.printToConsole(allGroups[0]);
   // changing the value of the first item in vector
   allGroups[0] = newGroup;
   // printed result woulb be different
-  newDeanery.printToConsole(*(allGroups[0]));
+  newDeanery.printToConsole(allGroups[0]);
   allGroups = newDeanery.getGroups();
   // but the original class object has not changed
-  newDeanery.printToConsole(*(allGroups[0]));
+  newDeanery.printToConsole(allGroups[0]);
 
   stats stats1 = newDeanery.getStatistics();
-  newDeanery.printToConsole(stats1);
+  newDeanery.printToConsole(&stats1);
 
   Student* newStudent = new Student(123456, "Monicheva Arina Aleksandrovna");
   for (int i = 0; i < 8; i++) {
-    newStudent->addMark(6 + rand_r(n) % 5);
+    newStudent->addMark(6 + rand_r(whyyy) % 5);
   }
 
   std::cout << newStudent->getAverageMark() << std::endl;
 
-  allGroups[0]->addStudent(*newStudent);
+  allGroups[0]->addStudent(newStudent);
   stats1 = newDeanery.getStatistics();
-  newDeanery.printToConsole(stats1);
+  newDeanery.printToConsole(&stats1);
 
-  newDeanery.initHeads(*(allGroups[0]));
-  newDeanery.printToConsole(*(allGroups[0]));
+  newDeanery.initHeads(allGroups[0]);
+  newDeanery.printToConsole(allGroups[0]);
   std::cout << newStudent->isHeadOfGroup() << std::endl;
 
   newDeanery.printToConsole(allGroups[0]->getStudent(123456));
@@ -54,20 +55,20 @@ int main() {
   << "Num in list: " <<
   allGroups[0]->containsStudent("Monicheva Arina Aleksandrovna") << std::endl;
 
-  newDeanery.moveStudents(*newStudent, *newGroup);
-  newDeanery.printToConsole(*newStudent);
+  newDeanery.moveStudents(newStudent, newGroup);
+  newDeanery.printToConsole(newStudent);
 
   stats1 = newDeanery.getStatistics();
-  newDeanery.printToConsole(stats1);
+  newDeanery.printToConsole(&stats1);
 
   newDeanery.addNewGroup(newGroup);
   stats1 = newDeanery.getStatistics();
-  newDeanery.printToConsole(stats1);
+  newDeanery.printToConsole(&stats1);
 
   newDeanery.fireStudents();
   allGroups = newDeanery.getGroups();
   for (int i = 0; i < allGroups.size(); i++) {
-    newDeanery.printToConsole(*(allGroups[i]));
+    newDeanery.printToConsole(allGroups[i]);
   }
 
   std::ofstream resafeSt("restudents.txt");
