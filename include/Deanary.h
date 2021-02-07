c1841ad55b6aa46009087819191298cf4f182745
GDM1705 <71228914+GDM1705@users.noreply.github.com>
2021-02-07 22:44:15 +0300

    Update Deanary.h

--- include/Deanary.h
+++ include/Deanary.h
@@ -13,16 +13,16 @@ private:
 	std::vector<Group*> groups;
 
 public:
-	void createGroups();
-	static std::vector<Student*> createStudents();
-	static void moveStudents(std::vector<Student*> students,
-		Group* from,
-		Group* to);
-	void addRandomMarksToAll();
-	void initHeads();
-	void getStatistics();
-	void saveStaff();
-	const std::vector<Group*>& getGroups() const;
+ void createGroups();
+  static std::vector<Student*> createStudents();
+static void moveStudents(std::vector<Student*> students,
+Group* from,
+Group* to);
+void addRandomMarksToAll();
+void initHeads();
+void getStatistics();
+void saveStaff();
+const std::vector<Group*>& getGroups() const;
 };
 
 #endif  // INCLUDE_DEANARY_H_
