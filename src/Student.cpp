3b4ed689c208dfa0d577abe07649bfbf9aa2487d
Schterben <dsbarinov_1@edu.hse.ru>
2021-02-07 23:29:40 +0300

    v1

---
+++ src/Student.cpp
@@ -0,0 +1,22 @@
+// Copyright 2021 DBarinov
+
+#include <Student.h>
+double Student::avMark() {
+    double sum = 0;
+    if (marks.empty()) {
+        return sum;
+    }
+    for (auto const& element : marks)
+        sum += element;
+    double res = sum / marks.size();
+    return res;
+}
+void Student::addMark(int m_) {
+    marks.push_back(m_);
+}
+int Student::getId() {
+    return id;
+}
+std::string Student::getName() {
+    return fio;
+}
