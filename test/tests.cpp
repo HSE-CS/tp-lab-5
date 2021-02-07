f6594e031da84ec18e5126344d33f1ed7668885d
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-02-07 18:27:51 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -17,3 +17,17 @@ TEST(PostfixTest, test2) {
   Student stud(21, fname);
   EXPECT_EQ(fname, stud.getFio());
 }
+
+TEST(PostfixTest, test3) {
+  std::string fname = "Shherbakov Aleksey";
+  Student stud(21, fname);
+  stud.addMark(6);
+  EXPECT_EQ(6, stud.getAveragemark());
+}
+
+TEST(PostfixTest, test4) {
+  std::string title = "19pmi2";
+  std::string spec = "PMI";
+  Group gr(title, spec);
+  EXPECT_EQ(title, gr.getTitle);
+}
