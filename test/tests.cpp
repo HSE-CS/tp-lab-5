a9404997b04e574852ca93cd4699b04e9f5f8103
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2021-02-07 18:32:50 +0300

    7 tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -29,5 +29,34 @@ TEST(PostfixTest, test4) {
   std::string title = "19pmi2";
   std::string spec = "PMI";
   Group gr(title, spec);
-  EXPECT_EQ(title, gr.getTitle);
+  EXPECT_EQ(title, gr.getTitle());
+}
+
+TEST(PostfixTest, test5) {
+  std::string title = "19pmi2";
+  std::string spec = "PMI";
+  Group gr(title, spec);
+  EXPECT_EQ(spec, gr.getSpec());
+}
+
+TEST(PostfixTest, test6) {
+  std::string title = "19pmi2";
+  std::string spec = "PMI";
+  Group gr(title, spec);
+  std::string fname = "Shherbakov Aleksey";
+  Student stud(21, fname);
+  stud.addMark(6);
+  gr.addStudent(&stud);
+  EXPECT_EQ(6, gr.getAveragemark_Gr());
+}
+
+TEST(PostfixTest, test7) {
+  std::string title = "19pmi2";
+  std::string spec = "PMI";
+  Group gr(title, spec);
+  std::string fname = "Shherbakov Aleksey";
+  Student stud(21, fname);
+  stud.addMark(6);
+  stud.addToGroup(&gr);
+  EXPECT_EQ(title, stud.getGroup()->getTitle());
 }
