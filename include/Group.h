4c851f5f66900409ec6929cf188ace7321cb598d
TimurZaytsev <tz200128@gmail.com>
2021-02-07 22:37:47 +0300

    sixth

--- include/Group.h
+++ include/Group.h
@@ -10,23 +10,23 @@ class Student;
 class Deanary;
 
 class Group {
-public:
-	double get_average_mark() const;
-	bool contains_student(uint64_t) const;
-	bool is_empty() const;
-	friend class Student;
-	friend class Deanary;
+ public:
+  double get_average_mark() const;
+  bool contains_student(uint64_t) const;
+  bool is_empty() const;
+  friend class Student;
+  friend class Deanary;
 
-private:
-	Student* get_student(uint64_t) const;
-	explicit Group(std::string);
-	Student* create_student(uint64_t, const std::string&);
-	void add_student(Student*);
-	void choose_head();
-	void remove_student(Student*);
-	std::vector<Student*> students;
-	std::string title;
-	Student* head;
+ private:
+  Student* get_student(uint64_t) const;
+  explicit Group(std::string);
+  Student* create_student(uint64_t, const std::string&);
+  void add_student(Student*);
+  void choose_head();
+  void remove_student(Student*);
+  std::vector<Student*> students;
+  std::string title;
+  Student* head;
 };
 
 #endif  // INCLUDE_GROUP_H_
