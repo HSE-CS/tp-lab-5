6f6ebfd93b58f7b5f735fced7618f7ae2e0f56ea
ZagoskinVA <zagoskin.3@mail.ru>
2021-02-07 17:17:01 +0300

    Fix cpplint

--- include/Deanary.h
+++ include/Deanary.h
@@ -10,23 +10,23 @@ class Student;
 class Group;
 
 class Deanary {
- private:
- std::vector<Group> groups;
- std::vector<Student> students;
- public:
- void createGroups(std::string path);
- void hireStudents(std::string path);
- void addMarkToAll();
- std::string getStatisticsByStudent(std::string fio);
- std::string getStatisticsByGroup(std::string title);
- void moveStudent(std::string fio, std::string title);
- void moveStudents();
- void initHead();
- void saveStuff(std::string path);
- void printInfo();
- void fireStudent();
- void addGroup(Group& group);
- std::vector<Group> getGroups();
- void addStudent(Student& student);
+private:
+    std::vector<Group> groups;
+    std::vector<Student> students;
+public:
+    void createGroups(std::string path);
+    void hireStudents(std::string path);
+    void addMarkToAll();
+    std::string getStatisticsByStudent(std::string fio);
+    std::string getStatisticsByGroup(std::string title);
+    void moveStudent(std::string fio, std::string title);
+    void moveStudents();
+    void initHead();
+    void saveStuff(std::string path);
+    void printInfo();
+    void fireStudent();
+    void addGroup(Group& group);
+    std::vector<Group> getGroups();
+    void addStudent(Student& student);
 };
 #endif // INCLUDE_DEANARY_H_
--- include/Group.h
+++ include/Group.h
@@ -8,23 +8,23 @@ class Student;
 
 class Group
 {
- private:
- std::string title;
- std::string spec;
- std::vector<Student> students;
- Student* head;
+private:
+    std::string title;
+    std::string spec;
+    std::vector<Student> students;
+    Student* head;
 
- public:
- Group(std::string title, std::string spec);
- void addStudent(Student& student);
- void chooseHead();
- Student& getStudent(int id);
- Student& getStudent(std::string fio);
- double getAverageMark();
- void removeStudent(Student& student);
- std::string getTitle();
- std::string getSpec();
- Student* getHead();
- std::vector<Student> getStudents();
+public:
+    Group(std::string title, std::string spec);
+    void addStudent(Student& student);
+    void chooseHead();
+    Student& getStudent(int id);
+    Student& getStudent(std::string fio);
+    double getAverageMark();
+    void removeStudent(Student& student);
+    std::string getTitle();
+    std::string getSpec();
+    Student* getHead();
+    std::vector<Student> getStudents();
 };
 #endif // INCLUDE_GROUP_H_
