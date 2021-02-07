f459355bd97351ec829d818bb1c1690c0ec1ffcc
NoName <ihackit595@gmail.com>
2021-02-07 22:48:32 +0300

    try to solve problem with input json filepath

--- test/tests.cpp
+++ test/tests.cpp
@@ -88,7 +88,7 @@ TEST(AllTests, test10) {
 
 TEST(AllTests, test11) {
     Deanery dn = Deanery();
-    std::vector<Group*> groups = dn.createGroupsFromFile("../src/input.json");
+    std::vector<Group*> groups = dn.createGroupsFromFile("./../src/input.json");
     EXPECT_EQ(groups[0]->getStudents()[0]->getFio(), "Marcia Herring");
 }
 
