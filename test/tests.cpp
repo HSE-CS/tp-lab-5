20bdab0a098caae2cf103f7ed8413948c1c6507d
SharunovEvgeny <Ee95023495>
2021-02-08 18:15:43 +0300

    fixStyle SHarunov

--- test/tests.cpp
+++ test/tests.cpp
@@ -26,31 +26,25 @@ TEST(test, test3) {
     Group* g = d.createGroup("Math", 12);
     EXPECT_EQ(g->isEmpty(), true);
 }
-TEST(test, test4) {
+
+TEST(test, test5) {
     Deanary d;
-    Student* s = d.hireStudent(1, "Шар. Ев. Алек.", "HR", 4);
-    d.initHeads();
-    EXPECT_EQ(true, s->isHeadGroup());
+    Group* g = d.createGroup("english language", 90);
+    d.hireStudent(302, "Шафин Евгений Вадимович", "english language", 90);
+    EXPECT_EQ(g->containsStudent(302), true);
 }
 
-//TEST(test, test5) {
-//    Deanary d;
-//    Group* g = d.createGroup("english language", 90);
-//    d.hireStudent(302, "Шафин Евгений Вадимович", "english language", 90);
-//    EXPECT_EQ(g->containsStudent(302), true);
-//}
-//
-//TEST(test, test6) {
-//    Deanary d;
-//    Group* g = d.createGroup("english language", 19);
-//    d.hireStudent(300, "Шарунов Евгений Вадимович", "english language", 24);
-//    EXPECT_EQ(g->containsStudent("Шарунов Евгений Вадимович"), false);
-//}
-//
-//TEST(test, test7) {
-//    Deanary d;
-//    Group* g = d.createGroup("english language", 21);
-//    d.hireStudent(303, "Шаломов Евгений Вадимович", "Programming", 25);
-//    d.transferStudent(303, 21);
-//    EXPECT_EQ(g->containsStudent(300), true);
-//}
+TEST(test, test6) {
+    Deanary d;
+    Group* g = d.createGroup("english language", 19);
+    d.hireStudent(300, "Шарунов Евгений Вадимович", "english language", 24);
+    EXPECT_EQ(g->containsStudent("Шарунов Евгений Вадимович"), false);
+}
+
+TEST(test, test7) {
+    Deanary d;
+    Group* g = d.createGroup("english language", 21);
+    d.hireStudent(303, "Шаломов Евгений Вадимович", "Programming", 25);
+    d.transferStudent(303, 21);
+    EXPECT_EQ(g->containsStudent(300), true);
+}
