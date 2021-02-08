744b7feeb90707a06ff17775696ad811f8bd0200
Lukicheva Polina <lukichevapolina42@gmail.com>
2021-02-08 12:30:38 +0300

    fix test

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,34 +8,34 @@
 #include "Group.h"
 
 TEST(Test, test1) {
-  Student student("1", "Lukicheva Polina Aleksandrovna");
+  Student student(1, "Lukicheva Polina Aleksandrovna");
   EXPECT_EQ(student.getFio(), "Lukicheva Polina Aleksandrovna");
 }
 
 TEST(Test, test2) {
-  Student st1("1", "Lukicheva Polina Aleksandrovna");
+  Student st1(1, "Lukicheva Polina Aleksandrovna");
   Group gr("SoftwareEngineering", "19SE1");
   gr.addStudent(st1);
   EXPECT_EQ(containsStudent(st1*), true);
 }
 
 TEST(Test, test3) {
-  Student student("1", "Lukicheva Polina Aleksandrovna");
+  Student student(1, "Lukicheva Polina Aleksandrovna");
   Group gr("SoftwareEngineering", "19SE1");
   gr.addStudent(student);
   EXPECT_EQ(gr.getStudent(1), "Lukicheva Polina Aleksandrovna");
 }
 
 TEST(Test, test4) {
-  Student student("1", "Lukicheva Polina Aleksandrovna");
+  Student student(1, "Lukicheva Polina Aleksandrovna");
   student.addmark(7);
   EXPECT_EQ(student.averageMark(), 7);
 }
 
 TEST(Test, test5) {
-  Student student1("1", "Lukicheva Polina Aleksandrovna");
+  Student student1(1, "Lukicheva Polina Aleksandrovna");
   student.addmark(7);
-  Student student2("2", "Katanova Evgeniya Ilinichna");
+  Student student2(2, "Katanova Evgeniya Ilinichna");
   student.addmark(7);
   Group gr("SoftwareEngineering", "19SE1");
   gr.addStudent(student1);
@@ -49,7 +49,7 @@ TEST(Test, test6) {
 }
 
 TEST(Test, test7) {
-  Student student("1", "Lukicheva Polina Aleksandrovna");
+  Student student(1, "Lukicheva Polina Aleksandrovna");
   Group gr("SoftwareEngineering", "19SE1");
   gr.addStudent(student);
   EXPECT_EQ(gr.isEmpty, false);
@@ -57,7 +57,7 @@ TEST(Test, test7) {
 
 TEST(Test, test8) {
   Deanary deanary;
-  Student student("1", "Lukicheva Polina Aleksandrovna");
+  Student student(1, "Lukicheva Polina Aleksandrovna");
   Group gr("SoftwareEngineering", "19SE1");
   gr.addStudent(student);
   deanary.addGroup(*gr);
@@ -72,7 +72,7 @@ TEST(Test, test9) {
 }
 
 TEST(Test, test10) {
-  Student student("1", "Lukicheva Polina Aleksandrovna");
+  Student student(1, "Lukicheva Polina Aleksandrovna");
   student.addmark(7);
   student.addmark(8);
   EXPECT_EQ(student.averageMark(), 7.5);
