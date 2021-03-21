75a0e812b7fc29f436a816a5f1f8a30e754b84f3
Mushka Nikita <namushka@edu.hse.ru>
2021-03-22 02:03:12 +0300

    test 1

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,15 @@
 // Copyright 2020 GHA Test Team
 
+#include "Deanary.h"
+#include "Group.h"
+#include "Student.h"
 #include <gtest/gtest.h>
 #include <string>
+#include <vector>
 
-
+TEST(Test, test1) {
+Student s1(1, "10 0");
+s1.AddMark(0);
+s1.AddMark(10);
+EXPECT_FLOAT_EQ(5, s1.getAverageMark());
+}
