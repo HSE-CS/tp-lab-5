f5a69e9a0f8c032b299bd72f300545b5500cea8d
skprpi <60091575+skprpi@users.noreply.github.com>
2021-03-25 14:26:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,16 @@
 
 
 int main() {
-    Deanery deanery;
-    deanery.hireStudents("data_students.txt");
-    deanery.createGroups("data_groups.txt");
-    deanery.initHeads();
-    for (int i = 0; i < 20; i++) {
-        deanery.addMarksToAll();
-    }
-    deanery.fireStudents();
-    deanery.getStatistics("statistic_data.txt");
-    deanery.saveStaff("data_groups.txt", "data_students.txt");
-    deanery.printData();
+    // Deanery deanery;
+    // deanery.hireStudents("data_students.txt");
+    // deanery.createGroups("data_groups.txt");
+    // deanery.initHeads();
+    // for (int i = 0; i < 20; i++) {
+    //     deanery.addMarksToAll();
+    // }
+    // deanery.fireStudents();
+    // deanery.getStatistics("statistic_data.txt");
+    // deanery.saveStaff("data_groups.txt", "data_students.txt");
+    // deanery.printData();
     return 0;
 }
