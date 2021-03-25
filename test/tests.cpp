b3d781439d34fd49a602577823799db3508d021b
skprpi <60091575+skprpi@users.noreply.github.com>
2021-03-25 14:58:30 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,21 +2,3 @@
 
 #include <gtest/gtest.h>
 #include <string>
-
-#include "Student.h"
-#include "Group.h"
-
-TEST(test, test1) {
-    int id = 5;
-    std::string fio = "Max Maximov";
-    Student student(id, fio);
-    EXPECT_EQ("Max Maximov", student.getFIO());
-}
-
-TEST(test, test2) {
-    int id = 5;
-    std::string fio = "Max Maximov";
-    Student student(id, fio);
-    EXPECT_EQ(5, student.getID());
-}
-
