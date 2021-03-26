7059fb74ce7b30ce8e40fe6e79ce89dd10506f14
Mushka Nikita <namushka@edu.hse.ru>
2021-03-26 23:26:16 +0300

    remake after release.yml fix

---
+++ include/Deanary.h
@@ -0,0 +1,42 @@
+// Copyright Mushka Nikita, 2021
+
+#ifndef INCLUDE_DEANARY_H_
+#define INCLUDE_DEANARY_H_
+
+#include "Group.h"
+#include "Student.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Deanary {
+ private:
+    std::vector<Group *> groups;
+
+ public:
+    void CreateGroup(std::string title, std::string spec);
+
+    static void CreateStudent(int new_id, std::string fio, Group *group);
+
+    Group *GroupByTitle(const std::string &groupName);
+
+    void FillMarks();
+
+    std::string GetStats();
+
+    static void MoveStudent(Student *student, Group *new_group);
+
+    void RemoveFailingStudents();
+
+    bool File_CreateGroups(const std::string &path);
+
+    bool File_CreateStudents(const std::string &path);
+
+    bool File_SaveDeanary(std::string path);
+
+    void ChooseLeaders();
+};
+
+
+#endif  // INCLUDE_DEANARY_H_
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+// Copyright Mushka Nikita, 2021
+
+#include <iostream>
+#include "../include/Deanary.h"
+
+int main() {
+    Deanary d;
+    d.File_CreateGroups("C:/Users/mushka/Desktop/tp-lab-5/src/data_groups.txt");
+    d.File_CreateStudents("C:/Users/mushka/Desktop/tp-lab-5/src/data_students.txt");
+    d.FillMarks();
+    d.ChooseLeaders();
+    std::cout << d.GetStats();
+    d.File_SaveDeanary("C:/Users/mushka/Desktop/tp-lab-5/src/save_file.txt");
+}
