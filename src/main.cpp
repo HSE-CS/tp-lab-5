b589c7c249322c3175e6a7fa0cfb88f50f9fa708
Mushka Nikita <namushka@edu.hse.ru>
2021-03-26 23:28:36 +0300

    quick cpplint fix

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 int main() {
     Deanary d;
     d.File_CreateGroups("C:/Users/mushka/Desktop/tp-lab-5/src/data_groups.txt");
-    d.File_CreateStudents("C:/Users/mushka/Desktop/tp-lab-5/src/data_students.txt");
+    d.File_CreateStudents(
+            "C:/Users/mushka/Desktop/tp-lab-5/src/data_students.txt");
     d.FillMarks();
     d.ChooseLeaders();
     std::cout << d.GetStats();
