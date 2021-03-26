58018facc002faa452d90368784ac8dbdf6e58b0
Polina1059 <polpan01@mail.ru>
2021-03-26 23:28:22 +0300

    changes

--- src/Deanary.cpp
+++ src/Deanary.cpp
@@ -98,7 +98,7 @@ void Deanery::electGhead() {
 void Deanery::getStatistics(std::string name) {
   for (int i = 0; i < numSt; i++) {
     StStat.push_back(
-        std::pair<float,std::string>(students[i]->averageMark(),
+        std::pair<float, std::string>(students[i]->averageMark(),
             students[i]->fio));
   }
   sort(StStat.begin(), StStat.end());
@@ -133,7 +133,8 @@ void Deanery::update(std::string name) {
       file << "In " << groups[i]->title << " " << groups[i]->num << " people. "
            << " Average score of the group: " << groups[i]->averageGroup()
            << std::endl;
-      file << "Average score of the group: " << groups[i]->head->fio << std::endl;
+      file << "Average score of the group: " << groups[i]->head->fio 
+          << std::endl;
       for (int j = 0; j < groups[i]->students.size(); j++) {
         file << groups[i]->students[j]->fio << "-"
              << groups[i]->students[j]->averageMark() << "   ";
@@ -156,7 +157,8 @@ void Deanery::printinfo() {
               << " people. "
               << " Average score of the group: " << groups[i]->averageGroup()
               << std::endl;
-    std::cout << "Average score of the group: " << groups[i]->head->fio << std::endl;
+    std::cout << "Average score of the group: " << groups[i]->head->fio 
+        << std::endl;
     for (int j = 0; j < groups[i]->students.size(); j++) {
       std::cout << groups[i]->students[j]->fio << "-"
            << groups[i]->students[j]->averageMark() << "    ";
