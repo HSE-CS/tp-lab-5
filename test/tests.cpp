9d78436f7e6ea61fb6d272f065e1ff09b758c04e
Shaidi <drsharibzhanova@edu.hse.ru>
2021-03-26 14:57:05 +0300

    test9 with some tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -8,21 +8,21 @@
 
 TEST(lab5, task1) {
   Student* st = new Student("Sharibzhanova Diana", 10);
-  st->addMark(3);
-  st->addMark(7);
+  st->add_mark(3);
+  st->add_mark(7);
   EXPECT_EQ(5.0, st->get_average_mark());
 }
 
 TEST(lab5, task2) {
   Student* st1 = new Student("Sharibzhanova Diana", 10);
   Student* st2 = new Student("Ivan Ivanov", 11);
-  st1->addMark(7);
-  st1->addMark(5);
-  st2->addMark(4);
-  st2->addMark(8);
+  st1->add_mark(7);
+  st1->add_mark(5);
+  st2->add_mark(4);
+  st2->add_mark(8);
   Group* gr = new Group("19PMI-3", "Prikladnaya Matematica");
-  gr->addStudent(st1);
-  gr->addStudent(st2);
+  gr->add_student(st1);
+  gr->add_student(st2);
   EXPECT_EQ(6.0, gr->average_group_mark());
 }
 
