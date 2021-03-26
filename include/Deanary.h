fca753d5c2699acdf4b6ac2e3705b9770f91e046
Shaidi <drsharibzhanova@edu.hse.ru>
2021-03-26 13:27:45 +0300

    test2

--- include/Deanary.h
+++ include/Deanary.h
@@ -1,7 +1,6 @@
 // Copyright 2021 shaidi
-
-#define DEANARY_H
-#ifdef DEANARY_H
+#define INCLUDE_DEANARY_H_
+#ifdef INCLUDE_DEANARY_H_
 #include <time.h>
 #include <iostream>
 #include "Group.h"
@@ -16,23 +15,19 @@ class Student;
 class Group;
 
 class Deanary {
-
  private:
   std::vector<Group*> groups;
   unsigned int dean_id;
 
  public:
-
   Deanary();
   void add_groups(std::string fios_input, std::string groups_input); 
   void change_group(std::string fio, std::string previous_group,
                     std::string new_group);  
-  void heads_choice();                       
+  void heads_choice();                 
   void set_random_marks(unsigned int num);
-  void get_statistics() const; 
-  void remove_students(); 
+  void get_statistics() const;
+  void remove_students();
   void new_data(std::string students, std::string groups) const; 
-
 };
-
 #endif  // DEANARY_H
--- include/Group.h
+++ include/Group.h
@@ -1,15 +1,12 @@
 // Copyright 2021 shaidi
 #ifndef INCLUDE_GROUP_H_
-#define INCLUDE_GROUP_H_ 
-
+#define INCLUDE_GROUP_H_
 #include <string>
 #include <vector>
-
 #include "Student.h"
 
 class Student;
 class Group {
-
  private:
   std::string title;
   std::string spec;
@@ -17,19 +14,16 @@ class Group {
   Student* head;
 
  public:
-
   explicit Group(std::string title, std::string spec);
-  std::string get_title() const;               
-  std::string get_spec() const;                
-  std::string get_name_of_head() const;        
-  std::vector<Student*> get_students() const;  
-  void add_student(Student* student); 
-  void remove_student(Student* student); 
-  void head_choice(); 
-  double average_group_mark() const; 
-  Student* search_by_fio(std::string fio) const;  
-  Student* search_by_id(int id) const;            
-
+  std::string get_title() const;             
+  std::string get_spec() const;            
+  std::string get_name_of_head() const;     
+  std::vector<Student*> get_students() const;
+  void add_student(Student* student);
+  void remove_student(Student* student);
+  void head_choice();
+  double average_group_mark() const;
+  Student* search_by_fio(std::string fio) const;
+  Student* search_by_id(int id) const;
 };
-
 #endif  // INCLUDE_GROUP_H_
